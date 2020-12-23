dd14614c8257d1efccddd73ef03e80bdf6480f0c
dsowerby <david.sowerby1@gmail.com>
2020-12-23 21:45:59 +0000

    Routes defined by map

--- lib/data/kitchenSink.dart
+++ lib/data/kitchenSink.dart
@@ -1,38 +1,41 @@
-
 import 'package:precept_script/script/backend.dart';
 import 'package:precept_script/script/help.dart';
 import 'package:precept_script/script/part/pString.dart';
 import 'package:precept_script/script/script.dart';
 import 'package:precept_script/script/style/style.dart';
 
-final kitchenSinkScript = PScript(name:'test',
-  components: {'core':
-  PComponent(
-    backend: PBackend(backendType: "mock", connection: {'id': 'mock1'}),
-    routes: { "/":
-    PRoute(
-      page: PPage(title: "Home Page", content: [
-        PPanel(
-          property: "",
-          caption: "Person",
-          help: PHelp(
-            title: "Display options",
-            message:
-            'All supported data types are shown.  To demonstrate different display options, some fields are shown multiple times',
+final kitchenSinkScript = PScript(
+  name: 'test',
+  components: {
+    'core': PComponent(
+      backend: PBackend(backendType: "mock", connection: {'id': 'mock1'}),
+      routes: {
+        "/": PRoute(
+          page: PPage(
+            title: "Home Page",
+            content: [
+              PPanel(
+                property: "",
+                caption: "Person",
+                help: PHelp(
+                  title: "Display options",
+                  message:
+                      'All supported data types are shown.  To demonstrate different display options, some fields are shown multiple times',
+                ),
+                heading: PPanelHeading(
+                  style: PHeadingStyle(background: PColor.primary),
+                ),
+                content: [
+                  PString(
+                    property: "firstName",
+                    caption: "First Name",
+                  ),
+                ],
+              ),
+            ],
           ),
-          heading: PPanelHeading(
-            style: PHeadingStyle(background: PColor.primary),
-          ),
-          content: [
-            PString(
-              property: "firstName",
-              caption: "First Name",
-            ),
-          ],
         ),
-      ]),
-    ),
-    },
-  )
+      },
+    )
   },
 );
