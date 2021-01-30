3e40cea3d3f7fdbb8ddf63ec86254c428ddb83e8
David Sowerby <david.sowerby1@gmail.com>
2021-01-30 18:55:49 +0000

    DocumentId held by Data

--- lib/backend/data.dart
+++ lib/backend/data.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/foundation.dart';
+import 'package:precept_script/script/documentId.dart';
 
 class Data {
+  final DocumentId documentId;
   final Map<String, dynamic> data;
 
-  const Data({@required this.data});
+  const Data({@required this.data,@required this.documentId});
 }
