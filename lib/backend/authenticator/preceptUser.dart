55f459761eef2c7e63a3870e3d2a62e6105fe4a9
David Sowerby <david.sowerby1@gmail.com>
2021-03-02 19:22:09 +0000

    Authenticator, PreceptUserConverter added

--- lib/backend/authenticator/preceptUser.dart
+++ lib/backend/authenticator/preceptUser.dart
@@ -9,3 +9,14 @@ class PreceptUser{
 
   PreceptUser({@required this.firstName, this.knownAs,@required this.lastName, this.email, @required this.userName});
 }
+
+/// If an SDK is used to provide a specific backend implementation it will need a way to convert their user
+/// class to Precept, and back.
+///
+/// The implementation of this needs to be added to GetIt in its register method. See :
+/// https://www.preceptblog.co.uk/developer-guide/backend-implementation.html#user-converter
+abstract class PreceptUserConverter<T>{
+
+  PreceptUser fromSDK(T sdkUser);
+  T toSDK (PreceptUser preceptUser);
+}
--- lib/backend/dataProvider/dataProviderLibrary.dart
+++ lib/backend/dataProvider/dataProviderLibrary.dart
@@ -4,13 +4,13 @@ import 'package:precept_script/common/exception.dart';
 import 'package:precept_script/common/log.dart';
 import 'package:precept_script/script/dataProvider.dart';
 
-/// A lookup facility for instances of [Backend] implementations.
-/// Provides an instance from the [find] method, from a supplied [PBackend]
-/// Also acts as a cache, as most [Backend] implementations are stateful and should be retained once
+/// A lookup facility for instances of [DataProvider] implementations.
+/// Provides an instance from the [find] method, from a supplied [PDataProvider]
+/// Also acts as a cache, as most [DataProvider] implementations are stateful and should be retained once
 /// connected
 ///
 /// Note: The original plan was to use GetIt for this, but there are a couple of obstacles to that approach:
-/// - the GetIt.registerFactory signature is type based, and does not fit for returning a Backend instance from a PBackend
+/// - the GetIt.registerFactory signature is type based, and does not fit for returning a DataProvider instance from a PDataProvider
 /// - the use of 'instanceName' is discouraged, although it is not clear why.
 class DataProviderLibrary {
   final Map<Type, DataProvider Function(PDataProvider)> builders = Map();
@@ -19,14 +19,14 @@ class DataProviderLibrary {
   DataProviderLibrary() : super();
 
   /// Finds an entry in the library matching [key], and returns an instance of it with [config].
-  /// [config] must be of the type appropriate to the [Backend] implementation required.
-  /// [config.instanceName] is only needed if you require two instances of the same [Backend] type.
+  /// [config] must be of the type appropriate to the [DataProvider] implementation required.
+  /// [config.instanceName] is only needed if you require two instances of the same [DataProvider] type.
   ///
   /// Throws a [PreceptException] if a builder for this config has not been registered
   DataProvider find({@required PDataProvider config}) {
     assert(config!=null);
     final lookupKey = '${config.runtimeType.toString()}:${config.instanceName}';
-    logType(this.runtimeType).d("Finding Backend for lookupKey: $lookupKey");
+    logType(this.runtimeType).d("Finding DataProvider for lookupKey: $lookupKey");
     if (instances.containsKey(lookupKey)) {
       return instances[lookupKey];
     }else{
@@ -43,7 +43,7 @@ class DataProviderLibrary {
 
 
 
-  /// Is there a way to check that [config] is a [PBackend] ?
+  /// Is there a way to check that [config] is a [PDataProvider] ?
   register({@required Type config, @required DataProvider Function(PDataProvider) builder}) {
     builders[config] = builder;
   }
