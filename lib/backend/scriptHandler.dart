55ae3714bd9158f64067ad4481645d9507a394a2
David Sowerby <david.sowerby1@gmail.com>
2021-05-17 10:11:18 +0100

    Null safety added to ScriptHandler

--- lib/backend/scriptHandler.dart
+++ lib/backend/scriptHandler.dart
@@ -2,8 +2,8 @@ import 'package:precept_script/schema/schema.dart';
 import 'package:precept_script/script/script.dart';
 
 abstract class ScriptHandler{
-  Future<bool> saveScript({PScript script});
-  Future<bool> saveSchema({PSchema script});
-  Future<Map<String,dynamic>> loadScript({String scriptName, int scriptVersion});
-  Future<Map<String,dynamic>> loadSchema({String schemaName, int schemaVersion});
+  Future<bool> saveScript({required PScript script});
+  Future<bool> saveSchema({required PSchema script});
+  Future<Map<String,dynamic>> loadScript({required String scriptName,required  int scriptVersion});
+  Future<Map<String,dynamic>> loadSchema({required String schemaName,required  int schemaVersion});
 }
