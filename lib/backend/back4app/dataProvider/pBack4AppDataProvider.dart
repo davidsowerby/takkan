ae3edafa57722c7f257d6173848a4bbbf90977bc
David Sowerby <david.sowerby1@gmail.com>
2021-07-25 21:16:33 +0100

    PBack4AppDataProvider was mutilated accidentally prior to last commit

--- lib/backend/back4app/dataProvider/pBack4AppDataProvider.dart
+++ lib/backend/back4app/dataProvider/pBack4AppDataProvider.dart
@@ -8,15 +8,16 @@ import 'package:precept_script/signin/signIn.dart';
 part 'pBack4AppDataProvider.g.dart';
 
 /// When [debug] enabled, prints logs to console
-@JsonSerializable(explicitToJson: true)
-
+///
 /// The server url is defined in *precept.json* and used in conjunction with some
 /// of the properties below. *precept.json* is loaded into AppConfig during application
 /// initialisation.
 ///
 /// [documentEndpoint] is the REST endpoint in relation to the server url
-/// [graphqlEndpoint] is the GraphQL endpoint in relation to the server url class PBack4AppDataProvider
-    extends PDataProvider {
+/// [graphqlEndpoint] is the GraphQL endpoint in relation to the server url class PBack4AppDataProvider.
+/// ----
+@JsonSerializable(explicitToJson: true)
+class PBack4AppDataProvider extends PDataProvider {
   PBack4AppDataProvider({
     PSignInOptions signInOptions = const PSignInOptions(),
     PSignIn signIn = const PSignIn(),
@@ -27,22 +28,21 @@ part 'pBack4AppDataProvider.g.dart';
     CloudInterface scriptDelegate = CloudInterface.graphQL,
     PSchema? schema,
   }) : super(
-    schema: schema,
-    signIn: signIn,
-    signInOptions: signInOptions,
-    configSource: configSource,
-    graphQLDelegate: PGraphQL(sessionTokenKey: 'sessionToken'),
-    restDelegate: PRest(sessionTokenKey: 'sessionToken'),
-    authenticatorDelegate: authenticatorDelegate,
-    headerKeys: const ['X-Parse-Application-Id', 'X-Parse-Client-Key'],
-    sessionTokenKey: 'sessionToken',
-  );
+          schema: schema,
+          signIn: signIn,
+          signInOptions: signInOptions,
+          configSource: configSource,
+          graphQLDelegate: PGraphQL(sessionTokenKey: 'sessionToken'),
+          restDelegate: PRest(sessionTokenKey: 'sessionToken'),
+          authenticatorDelegate: authenticatorDelegate,
+          headerKeys: const ['X-Parse-Application-Id', 'X-Parse-Client-Key'],
+          sessionTokenKey: 'sessionToken',
+        );
 
   String get idPropertyName => 'objectId';
+
   factory PBack4AppDataProvider.fromJson(Map<String, dynamic> json) =>
       _$PBack4AppDataProviderFromJson(json);
 
   Map<String, dynamic> toJson() => _$PBack4AppDataProviderToJson(this);
-
-
 }
