b451378df46985be0b0d6af95f67d4779443b64e
David Sowerby <david.sowerby1@gmail.com>
2022-01-22 11:35:20 +0000

    Schema generation moved to dev_app

--- lib/backend/schema/serverCode/serverCode.dart
+++
@@ -1,7 +0,0 @@
-import 'package:precept_script/schema/schema.dart';
-
-abstract class ServerCodeGenerator {
-  bool generate({required PSchema schema});
-
-  Future<bool> exportCode({required String exportPath});
-}
